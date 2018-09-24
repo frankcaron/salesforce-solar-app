2aed3a06726b2364882097b5d827913a8d1d22d3
Frank Caron <frank@frankcaron.com>
2018-09-24 23:05:45 +0000

    Init node commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+/*
+==============================================================
+SalesForce Solar App
+
+Written by Frank Caron
+September 2018
+==============================================================
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
